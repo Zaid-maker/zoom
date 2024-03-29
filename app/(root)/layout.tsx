810ab962411216ee78179c1ebc14ec3575ed96a1
DevMirza <53424436+Zaid-maker@users.noreply.github.com>
2024-03-29 13:51:07 +0000

    File folder structure
    Co-authors: @devmirza-bot

---
+++ app/(root)/layout.tsx
@@ -0,0 +1,7 @@
+import React, { ReactNode } from "react";
+
+const RootLayout = ({ children }: Readonly<{ children: ReactNode }>) => {
+  return <main>{children}</main>;
+};
+
+export default RootLayout;
