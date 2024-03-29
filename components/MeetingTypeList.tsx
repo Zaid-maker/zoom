3641323dd82a0122d8a87a42376c3deec2ab64ef
DevMirza <53424436+Zaid-maker@users.noreply.github.com>
2024-03-29 20:29:45 +0000

    update HomeCard component props

--- components/MeetingTypeList.tsx
+++ components/MeetingTypeList.tsx
@@ -6,10 +6,29 @@ import HomeCard from "./HomeCard";
 const MeetingTypeList = () => {
   return (
     <section className="grid grid-cols-1 gap-5 md:grid-cols-2 xl:grid-cols-4">
-      <HomeCard />
-      <HomeCard />
-      <HomeCard />
-      <HomeCard />
+      <HomeCard
+        img="/icons/add-meeting.svg"
+        title="New Meeting"
+        description="Start and instant meeting"
+      />
+      <HomeCard
+        img="/icons/join-meeting.svg"
+        title="Join Meeting"
+        description="Join via invitation link"
+        className="bg-blue-1"
+      />
+      <HomeCard
+        img="/icons/schedule.svg"
+        title="Schedule"
+        description="Plan your meetings with schedules"
+        className="bg-purple-1"
+      />
+      <HomeCard
+        img="/icons/recordings.svg"
+        title="Recordings"
+        description="View Recordings of Meetings"
+        className="bg-yellow-1"
+      />
     </section>
   );
 };
