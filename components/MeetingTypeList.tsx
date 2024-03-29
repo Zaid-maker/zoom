00d6bac8701ed14e1fc5f10a5cd352537298329b
DevMirza <53424436+Zaid-maker@users.noreply.github.com>
2024-03-29 20:24:25 +0000

    MeetingTypeList compoent

---
+++ components/MeetingTypeList.tsx
@@ -0,0 +1,17 @@
+"use client";
+
+import React from "react";
+import HomeCard from "./HomeCard";
+
+const MeetingTypeList = () => {
+  return (
+    <section className="grid grid-cols-1 gap-5 md:grid-cols-2 xl:grid-cols-4">
+      <HomeCard />
+      <HomeCard />
+      <HomeCard />
+      <HomeCard />
+    </section>
+  );
+};
+
+export default MeetingTypeList;
