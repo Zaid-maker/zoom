9442a4cc90ce73e69105182345cd75a02bc38fb3
DevMirza <53424436+Zaid-maker@users.noreply.github.com>
2024-03-29 21:52:03 +0000

    reusable MeetingModel

--- components/MeetingTypeList.tsx
+++ components/MeetingTypeList.tsx
@@ -3,12 +3,15 @@
 import React, { useState } from "react";
 import HomeCard from "./HomeCard";
 import { useRouter } from "next/navigation";
+import MeetingModel from "./MeetingModel";
 
 const MeetingTypeList = () => {
   const router = useRouter();
   const [meetingState, setMeetingState] = useState<
     "isScheduleMeeting" | "isJoiningMeeting" | "isInstantMeeting" | undefined
   >(undefined);
+
+  const createMeeting = () => {};
 
   return (
     <section className="grid grid-cols-1 gap-5 md:grid-cols-2 xl:grid-cols-4">
@@ -38,6 +41,15 @@ const MeetingTypeList = () => {
         description="View Recordings of Meetings"
         className="bg-yellow-1"
         handleClick={() => router.push("/recordings")}
+      />
+
+      <MeetingModel
+        isOpen={meetingState === "isInstantMeeting"}
+        onClose={() => setMeetingState(undefined)}
+        title="Start an Instant Meeting"
+        className="text-center"
+        buttonText="Start Meeting"
+        handleClick={createMeeting}
       />
     </section>
   );
