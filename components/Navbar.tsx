25fcd5d3130fd90f331e76220218db1f2fb1bc1f
DevMirza <53424436+Zaid-maker@users.noreply.github.com>
2024-03-29 17:30:06 +0000

    Show signed in user in nav

--- components/Navbar.tsx
+++ components/Navbar.tsx
@@ -2,6 +2,7 @@ import Image from "next/image";
 import Link from "next/link";
 import React from "react";
 import MobileNav from "./MobileNav";
+import { SignedIn, UserButton } from "@clerk/nextjs";
 
 const Navbar = () => {
   return (
@@ -19,7 +20,10 @@ const Navbar = () => {
         </p>
       </Link>
       <div className="flex-between gap-5 text-white">
-        SIGN IN
+        <SignedIn>
+          <UserButton afterSignOutUrl="/" />
+        </SignedIn>
+
         <MobileNav />
       </div>
     </div>
