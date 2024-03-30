371d8826b604dfe2956ca38279270b5a2370fd6e
DevMirza <53424436+Zaid-maker@users.noreply.github.com>
2024-03-30 21:45:09 +0000

    try catch

--- components/MeetingTypeList.tsx
+++ components/MeetingTypeList.tsx
@@ -11,7 +11,12 @@ const MeetingTypeList = () => {
     "isScheduleMeeting" | "isJoiningMeeting" | "isInstantMeeting" | undefined
   >(undefined);
 
-  const createMeeting = () => {};
+  const createMeeting = () => {
+    try {
+    } catch (error) {
+      console.error(error);
+    }
+  };
 
   return (
     <section className="grid grid-cols-1 gap-5 md:grid-cols-2 xl:grid-cols-4">
