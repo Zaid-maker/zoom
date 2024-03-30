2b6bf1855c804ccc6eb5d029e937c333b127db2e
DevMirza <53424436+Zaid-maker@users.noreply.github.com>
2024-03-30 22:26:17 +0000

    Loader

--- providers/StreamClientProvider.tsx
+++ providers/StreamClientProvider.tsx
@@ -5,7 +5,7 @@ import { StreamVideoClient, StreamVideo } from "@stream-io/video-react-sdk";
 import { useUser } from "@clerk/nextjs";
 
 // import { tokenProvider } from '@/actions/stream.actions';
-// import Loader from '@/components/Loader';
+import Loader from "@/components/Loader";
 
 const API_KEY = process.env.NEXT_PUBLIC_STREAM_API_KEY;
 
