542815219d8d9ce4c6ea8ae8f5ce88bb2084987c
DevMirza <53424436+Zaid-maker@users.noreply.github.com>
2024-04-05 21:36:19 +0000

    update meeting id

--- app/(root)/meeting/[id]/page.tsx
+++ app/(root)/meeting/[id]/page.tsx
@@ -1,9 +1,27 @@
 "use client";
+import { useUser } from "@clerk/nextjs";
+import { useParams } from "next/navigation";
+import { useState } from "react";
+import { StreamCall, StreamTheme } from "@stream-io/video-react-sdk";
+// import { useGetCallById } from '@/hooks/useGetCallById';
 
-import React from "react";
+import Loader from "@/components/Loader";
 
 const MeetingPage = () => {
-  return <div>MeetingPage</div>;
+  const { id } = useParams();
+  const { isLoaded, user } = useUser();
+  //const { call, isCallLoading } = useGetCallById(id);
+  const [isSetupComplete, setIsSetupComplete] = useState(false);
+
+  if (!isLoaded) return <Loader />;
+
+  return (
+    <main className="h-screen w-full">
+      <StreamCall>
+        <StreamTheme></StreamTheme>
+      </StreamCall>
+    </main>
+  );
 };
 
 export default MeetingPage;
