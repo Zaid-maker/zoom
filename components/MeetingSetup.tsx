6925f637b2bed86be784a9593660591d1101679f
DevMirza <53424436+Zaid-maker@users.noreply.github.com>
2024-04-07 21:25:49 +0000

    update MeetingSetup to toggle mic and camera

--- components/MeetingSetup.tsx
+++ components/MeetingSetup.tsx
@@ -1,26 +1,67 @@
-import { DeviceSettings } from "@stream-io/video-react-sdk";
-import React from "react";
+"use client";
+
+import {
+  DeviceSettings,
+  useCall,
+  useCallStateHooks,
+} from "@stream-io/video-react-sdk";
+import React, { useEffect, useState } from "react";
 import { Button } from "./ui/button";
 
-const MeetingSetup = () => {
+const MeetingSetup = ({
+  setIsSetupComplete,
+}: {
+  setIsSetupComplete: (value: boolean) => void;
+}) => {
+  const { useCallEndedAt, useCallStartsAt } = useCallStateHooks();
+  const callStartsAt = useCallStartsAt();
+  const callEndedAt = useCallEndedAt();
+  const callTimeNotArrived =
+    callStartsAt && new Date(callStartsAt) > new Date();
+  const callHasEnded = !!callEndedAt;
+
+  const call = useCall();
+
+  if (!call) {
+    throw new Error(
+      "useStreamCall must be used within a StreamCall component."
+    );
+  }
+
+  const [isMicCamToggled, setIsMicCamToggled] = useState(false);
+
+  useEffect(() => {
+    if (isMicCamToggled) {
+      call.camera.disable();
+      call.microphone.disable();
+    } else {
+      call.camera.enable();
+      call.microphone.enable();
+    }
+  }, [isMicCamToggled, call.camera, call.microphone]);
+
   return (
     <div className="flex h-screen w-full flex-col items-center justify-center gap-3 text-white">
       <h1 className="text-center text-2xl font-bold">Setup</h1>
       VideoPreview
       <div className="flex h-16 items-center justify-center gap-3">
         <label className="flex items-center justify-center gap-2 font-medium">
-          <input type="checkbox" />
+          <input
+            type="checkbox"
+            checked={isMicCamToggled}
+            onChange={(e) => setIsMicCamToggled(e.target.checked)}
+          />
           Join with Mic and Camera Off
         </label>
         <DeviceSettings />
       </div>
       <Button
         className="rounded-md bg-green-500 px-4 py-2.5"
-        //onClick={() => {
-        //  call.join();
+        onClick={() => {
+          call.join();
 
-        //  setIsSetupComplete(true);
-        //}}
+          setIsSetupComplete(true);
+        }}
       >
         Join meeting
       </Button>
