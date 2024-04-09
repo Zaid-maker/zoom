94f8f02a0a778fb582841982f32ef60d834e0c6e
DevMirza <53424436+Zaid-maker@users.noreply.github.com>
2024-04-09 19:28:00 +0000

    use  style from stream

--- app/layout.tsx
+++ app/layout.tsx
@@ -1,8 +1,11 @@
 import type { Metadata } from "next";
 import { Inter } from "next/font/google";
 import { ClerkProvider } from "@clerk/nextjs";
-import "./globals.css";
 import { Toaster } from "@/components/ui/toaster";
+
+import "@stream-io/video-react-sdk/dist/css/styles.css";
+//  import "react-datepicker/dist/react-datepicker.css";
+import "./globals.css";
 
 const inter = Inter({ subsets: ["latin"] });
 
